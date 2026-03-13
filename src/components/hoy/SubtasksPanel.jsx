54c98e90e146ff5e19ea706228c19ba01ff61c2a
miguelyoos <miguelosoriohijo@gmail.com>
2026-03-12 21:34:01 -0500

    recolor

--- src/components/hoy/SubtasksPanel.jsx
+++ src/components/hoy/SubtasksPanel.jsx
@@ -64,7 +64,7 @@ export default function SubtasksPanel({ activity, open, onAddSubtask, onEditSubt
                     title="Editar"
                     onClick={() => onEditSubtask(activity.id, s.id)}
                   >
-                    <svg width="12" height="12" fill="none" viewBox="0 0 24 24" strokeWidth="2.5">
+                    <svg width="12" height="12" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor">
                       <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
                       <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
                     </svg>
@@ -74,7 +74,7 @@ export default function SubtasksPanel({ activity, open, onAddSubtask, onEditSubt
                     title="Eliminar"
                     onClick={() => handleDeleteClick(s)}
                   >
-                    <svg width="12" height="12" fill="none" viewBox="0 0 24 24" strokeWidth="2.5">
+                    <svg width="12" height="12" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor">
                       <polyline points="3 6 5 6 21 6" />
                       <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
                       <path d="M10 11v6" /><path d="M14 11v6" />
