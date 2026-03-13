60c8e2ae13f9e8fb6dc01324f5b9461e4e292ce7
miguelyoos <miguelosoriohijo@gmail.com>
2026-03-12 21:37:18 -0500

    iconos

--- src/components/hoy/SubtasksPanel.jsx
+++ src/components/hoy/SubtasksPanel.jsx
@@ -64,9 +64,8 @@ export default function SubtasksPanel({ activity, open, onAddSubtask, onEditSubt
                     title="Editar"
                     onClick={() => onEditSubtask(activity.id, s.id)}
                   >
-                    <svg width="12" height="12" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor">
-                      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"/>
                     </svg>
                   </button>
                   <button
@@ -74,10 +73,11 @@ export default function SubtasksPanel({ activity, open, onAddSubtask, onEditSubt
                     title="Eliminar"
                     onClick={() => handleDeleteClick(s)}
                   >
-                    <svg width="12" height="12" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor">
-                      <polyline points="3 6 5 6 21 6" />
-                      <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
-                      <path d="M10 11v6" /><path d="M14 11v6" />
+                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <polyline points="3 6 5 6 21 6"/>
+                      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+                      <line x1="10" y1="11" x2="10" y2="17"/>
+                      <line x1="14" y1="11" x2="14" y2="17"/>
                     </svg>
                   </button>
                 </div>
